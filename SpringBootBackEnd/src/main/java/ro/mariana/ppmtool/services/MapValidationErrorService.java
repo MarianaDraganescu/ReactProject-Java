8bbaae4d02dfd7f7ff2a0b2f1dbe6e3d2c4aa178
MarianaDraganescu <draganescu.mariana@yahoo.com>
2020-08-29 17:51:47 +0300

    refactored code error validation

---
+++ SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/services/MapValidationErrorService.java
@@ -0,0 +1,28 @@
+package ro.mariana.ppmtool.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> mapValidation(BindingResult result){
+
+        if(result.hasErrors()){
+
+            final Map<String,String> errorMap = new HashMap<>();
+            for(final FieldError error : result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String,String>> (errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
--- SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/web/ProjectController.java
+++ SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/web/ProjectController.java
@@ -3,39 +3,35 @@ package ro.mariana.ppmtool.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.mariana.ppmtool.domain.Project;
+import ro.mariana.ppmtool.services.MapValidationErrorService;
 import ro.mariana.ppmtool.services.ProjectService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final MapValidationErrorService mapValidationErrorService;
 
-    public ProjectController(final ProjectService projectService) {
+    public ProjectController(final ProjectService projectService,
+                             MapValidationErrorService mapValidationErrorService) {
         this.projectService = projectService;
+        this.mapValidationErrorService = mapValidationErrorService;
     }
 
     @PostMapping("")
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody final Project project, final BindingResult result){
-        if(result.hasErrors()){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody final Project project, BindingResult result) {
 
-            final Map<String,String> errorMap = new HashMap<>();
-            for(final FieldError error : result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
 
-            return new ResponseEntity<Map<String,String>> (errorMap,HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidation(result);
+        if (errorMap != null) return errorMap;
 
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
