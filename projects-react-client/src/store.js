db1bf5c9f01c0e21f3855c574947dc734f43358e
MarianaDraganescu <draganescu.mariana@yahoo.com>
2020-09-12 18:11:22 +0300

    Added dev-tools package with npm and connected to redux store

--- projects-react-client/src/store.js
+++ projects-react-client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -7,21 +8,22 @@ const middleware = [thunk];
 
 let store;
 
-if(window.navigator.userAgent.includes("Crome")){
-
-    store = createStore(
-        rootReducer,
-        initialState,
-        compose(applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()   ));
-} else {
-    store = createStore(
-        rootReducer,
-        initialState,
-        compose(applyMiddleware(...middleware))
+if (window.navigator.userAgent.includes("Crome")) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     )
-
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
 }
 
-export  default store;
+export default store;
