e245d05e3c80b860127b797bcc179820bef554fa
MarianaDraganescu <draganescu.mariana@yahoo.com>
2021-02-01 14:36:26 +0200

    create custom login errors and create authentification connection

---
+++ SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package ro.mariana.ppmtool.security;
+
+import com.google.gson.Gson;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+/*
+We use this class if a user doesn't have the right credentials
+and to display proper error messages
+ */
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponde = new Gson().toJson(loginResponse);
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().print(jsonLoginResponde);
+    }
+}
---
+++ SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package ro.mariana.ppmtool.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()  //we use token and we don't need that to be enabled
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  //we are using tokens,so we don't need to store the state(the state is sotred in Redux) or use cookies
+                .and()
+                .headers().frameOptions().sameOrigin() // only if we use h2-in memory db,otherwise we don't need this line of code
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                        ).permitAll()
+                .anyRequest().authenticated();
+    }
+}
