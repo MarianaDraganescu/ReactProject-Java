d83958f7e5e06d4f5e2970502f97b6f407cf6878
MarianaDraganescu <draganescu.mariana@yahoo.com>
2021-02-01 22:23:59 +0200

    added custom exception if username already exists

--- SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ SpringBootBackEnd/src/main/java/ro/mariana/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -19,8 +19,14 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     }
 
     @ExceptionHandler
-    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request){
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request) {
         ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
-        return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex, WebRequest request) {
+        UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
